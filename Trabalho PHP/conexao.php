6fa5ded3106bd8a441c640acfe69bb060c00004a
nickoolaz <nicolasmarceloaraujo@gmail.com>
2022-09-06 21:23:43 -0300

    Conexão com o banco de dados

---
+++ Trabalho PHP/conexao.php
@@ -0,0 +1,9 @@
+<?php
+    $endereco = 'localhost';
+    $usuario = 'root';
+    $senha = '';
+    $base = 'unicv_web';
+    
+    $conexao = mysqli_connect('localhost', 'root', '', 'unicv_web');
+    var_dump($conexao);
+?>
