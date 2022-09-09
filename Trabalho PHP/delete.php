bfb010fc036c13e60947f1aacb3bb2804714fafd
Dieyson <dieysonjason@gmail.com>
2022-09-09 17:45:53 -0300

    Inserção de imagens no background

--- Trabalho PHP/delete.php
+++ Trabalho PHP/delete.php
@@ -7,6 +7,20 @@
    <title>Excluir Produtos</title>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
    <src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></>    
+   <style>
+     body{
+          background-image: url(http://www.blogdosupermercado.com.br/wp-content/uploads/2018/03/food-2179178_1280_prateleiras_supermercado-e1520432501121.jpg);
+          background-size:cover;
+     }
+     .btn{
+          font-size: 35px;
+          color:light;
+          width: 300px;
+          height: 80px;
+          margin-top: 350px;
+          margin-left:550px ;    
+     }
+   </style>
  </head>
  <body>
  <?php
@@ -17,7 +31,7 @@
     mysqli_close($conexao);
  ?>
 
- <a href="select.php" class="btn btn-primary">Produtos</a>    
+ <a href="select.php" class="btn btn-dark">Produtos</a>    
  
  </body>
  </html>
