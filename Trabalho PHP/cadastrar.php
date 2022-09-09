a1888f1625112a476f5be87078f2d7dd3e65d242
Dieyson <dieysonjason@gmail.com>
2022-09-09 17:45:53 -0300

    inserção de imagens para o backgraound

--- Trabalho PHP/cadastrar.php
+++ Trabalho PHP/cadastrar.php
@@ -6,6 +6,16 @@
     <title>Cadastrar Produtos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>    
+    <style>
+       body{
+          background-image:url(http://www.blogdosupermercado.com.br/wp-content/uploads/2018/08/chuttersnap-496714-unsplash.jpg);
+          background-size:cover;
+     }
+     .card{
+          opacity:0.8;
+          color:orange;
+     } 
+    </style>
   </head>
   <body>
     <div class="card m-4">
--- Trabalho PHP/update.php
+++ Trabalho PHP/update.php
@@ -7,6 +7,20 @@
      <title>Atualizar Produtos</title>
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+     <style>
+     body{
+          background-image: url(https://thumbs.dreamstime.com/z/uma-variedade-de-alimentos-livres-saud%C3%A1veis-e-do-a%C3%A7%C3%BAcar-em-uma-prateleira-32092484.jpg);
+          background-size:cover;
+     }
+     .btn{
+          font-size: 35px;
+          color:light;
+          width: 300px;
+          height: 80px;
+          margin-top: 350px;
+          margin-left:550px ;    
+     }
+   </style>
 </head>
 <body>
 <?php
@@ -22,7 +36,7 @@
      
      mysqli_close($conexao);
 ?>
-<a href="select.php" class="btn btn-primary">Produtos</a>
+<a href="select.php" class="btn btn-dark">Produtos</a>
 
 </body>
 </html>
